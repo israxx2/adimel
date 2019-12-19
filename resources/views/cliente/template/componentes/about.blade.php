e6dd39ea6fbbd380615bdc1508e88bd6a4624ff4
israxx2 <israxx2@gmail.com>
2019-12-19 16:28:23 -0300

    Quienes somos y otros

---
+++ resources/views/cliente/template/componentes/about.blade.php
@@ -0,0 +1,24 @@
+<div class="about-us-wrapper pt-60 pb-40">
+    <div class="container">
+        <div class="row">
+            <!-- About Text Start -->
+            <div class="col-lg-6 order-last order-lg-first">
+                <div class="about-text-wrap">
+                    <h2><span>¿Quiénes Somos?</span>Librería Adimel</h2>
+                    <p><strong>Misión</strong></p>
+                    <p>Desarrollar una relación a largo plazo con nuestro público a través de una atención personalizada, precios asequibles y gran variedad de artículos escolares o de oficina. Privilegiando los distintos intereses que puedan tener nuestros clientes de acuerdo a sus requerimientos. </p>
+                    <p><strong>Visión</strong></p>
+                    <p>Trabajar de la mano con nuestros clientes para ofrecerles un mejor trato lleno de amabilidad y dedicación, con el fin de innovar e impactar en cuanto a nuestras soluciones, servicios y productos. </p>        
+                </div>
+            </div>
+            <!-- About Text End -->
+            <!-- About Image Start -->
+            <div class="col-lg-5 col-md-10">
+                <div class="about-image-wrap">
+                    <img class="img-full" src="{{ asset('electro/images/about-us/adimel.jpg') }}" alt="About Us" />
+                </div>
+            </div>
+            <!-- About Image End -->
+        </div>
+    </div>
+</div>
